ffd177713ba0513cf63f233ee40635c1fa569da3
Aishwarya Ghorpade <aishwaryaghorpade4@gmail.com>
2024-10-23 16:44:27 +0530

    Complete Queue

---
+++ Queue/TwoStackQueue.java
@@ -0,0 +1,59 @@
+//Queue implementation using 2 stacks
+
+package Queue;
+import java.util.*;
+public class TwoStackQueue {
+     
+    static class Queue{
+        static Stack<Integer> s1=new Stack<>();
+        static Stack<Integer> s2=new Stack<>();
+        //isEmpty
+        public static boolean isEmpty(){
+            return s1.isEmpty();
+        }
+
+        //add
+        public static void add(int data)
+        {
+            while(!s1.isEmpty())
+            {
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+        public static int remove()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue Empty");
+                return -1;
+            }
+            return s1.pop();
+        }
+        public static int peek()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue Empty");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+    public static void main(String[] args) {
+        Queue q=new Queue();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        while(!q.isEmpty())
+        {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
